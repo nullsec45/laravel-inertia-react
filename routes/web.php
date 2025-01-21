92732094dd1db30bcdca5b75beb2e5d86bff5e31
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-21 22:22:07 +0700

    modified : removed unused route.

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    sleep(2);
-    return inertia('Home',['name' => 'Fajar']);
-});
 
 Route::get('/', [PostController::class,'index']);
 Route::resource('posts',PostController::class)->except('index');
