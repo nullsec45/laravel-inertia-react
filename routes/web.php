070987f71427ed1ae570852f941e35df0391196b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-26 14:48:27 +0700

    modified : add name route home.

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [PostController::class,'index']);
+Route::get('/', [PostController::class,'index'])->name('/');
 Route::resource('posts',PostController::class)->except('index');
