0498663c98da59f3bb5d2f4aebf3623505b1c110
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-30 23:34:49 +0700

    modified : add button edit in show page.

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,6 +1,5 @@
 import Layouts from "../Layouts/Layouts"
-import { useForm } from '@inertiajs/react'
-
+import { useForm,Link } from '@inertiajs/react'
 
 export default function Show({post}){
     const {delete:destroy}=useForm();
@@ -18,12 +17,15 @@ export default function Show({post}){
                     <span>{new Date(post.created_at).toLocaleTimeString()}</span>
                 </div>
                 <p className="font-medium">{post.body}</p>
-                <div className="flex items-center justify-end gap-2">
+                <div className="flex items-center justify-end gap-2 mt-4">
                     <form onSubmit={submit}>
                         <button className="bg-red-500 rounded-md text-sm px-4 py-1 text-white">
                             Delete
                         </button>
                     </form>
+                    <Link href={`/posts/${post.id}/edit`} className="bg-green-500 rounded-md text-sm px-4 py-1 text-white">
+                    Edit
+                     </Link>
                 </div>
             </div>
         </>
