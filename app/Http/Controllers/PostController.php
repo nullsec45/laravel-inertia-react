d0862a72ae949fe1b3a561812874c82074e358b2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-30 23:32:43 +0700

    modified : update post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -57,7 +57,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return inertia("Edit",["post" => $post]);
     }
 
     /**
@@ -65,7 +65,15 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        sleep(2);
+
+        $fields=$request->validate([
+            'body' => ['required']
+        ]);
+
+        $post->update($fields);
+
+        return redirect('/')->with('success','The post was updated successfully.');
     }
 
     /**
